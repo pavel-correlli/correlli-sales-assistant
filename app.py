a78db8e3d0b3fe24a76cf3e8408fc2bc76c7f5e4
pavel-correlli <pavel@correlli.engineering>
2026-01-12 22:01:53 +0400

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ from datetime import datetime, timedelta
 import supabase
 from supabase import create_client, Client
 
-# Supabase connection
-SUPABASE_URL = st.secrets["supabase"]["url"]
-SUPABASE_KEY = st.secrets["supabase"]["key"]
+@st.cache_resource
+def init_connection():
+    return create_client(st.secrets["SUPABASE_URL"], st. secrets["SUPABASE_KEY"])
 supabase_client: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 # Page configuration
