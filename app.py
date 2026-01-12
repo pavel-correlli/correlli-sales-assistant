96c309addd995f70bac67e90b956720ea0d32d92
pavel-correlli <pavel@correlli.engineering>
2026-01-12 22:35:37 +0400

    Improve formatting of init_connection function
    Refactor init_connection function for better readability.

--- app.py
+++ app.py
@@ -14,7 +14,12 @@ st.set_page_config(
 )
 @st.cache_resource
 def init_connection():
-    return create_client(st.secrets["SUPABASE_URL"], st.secrets["SUPABASE_KEY"])
+    return create_client(
+        st.secrets["SUPABASE_URL"],
+        st.secrets["SUPABASE_KEY"]
+    )
+
+supabase = init_connection()
 
 
 MARKETS_ALLOWED = ["CZ", "RUK", "SK"]
